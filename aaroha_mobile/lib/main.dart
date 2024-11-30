6fe528adfadd8d0d95bba958b8ea27a4bb0374c8
Aditya Donapati <adityamdonapati@gmail.com>
2024-12-01 02:59:33 +0530

    home route in main

--- aaroha_mobile/lib/main.dart
+++ aaroha_mobile/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:aaroha_mobile/landingpage.dart';
 import 'package:aaroha_mobile/signin_screen.dart';
 import 'package:aaroha_mobile/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 import 'calculatorscreen.dart';  // Import the CalculatorScreen
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -26,7 +27,8 @@ class MyApp extends StatelessWidget {
         '/': (context) => LandingPage(),
         '/signin': (context) => SignInScreen(),
         '/signup': (context) => SignUpScreen(),
-        '/calculator': (context) => CalculatorScreen(), // Your Calculator screen
+        '/calculator': (context) => CalculatorScreen(),
+        '/home': (context) => HomeScreen()
       },
     );
   }
